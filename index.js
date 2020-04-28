2aaf484ee6b28469d10a8f8b5752bd1eb8c0eeb6
Samantha Levin <samantha@Samanthas-MacBook-Pro-4.local>
2020-04-28 14:06:36 -0400

    added population data

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const state_abbrevs = {"Alabama":"AL", "Alaska":"AK", "Arizona":"AZ", "Arkansas":"AR", "California":"CA", "Colorado":"CO", "Connecticut":"CT", "Delaware":"DE", "District of Columbia":"DC", "Florida":"FL", "Georgia":"GA", "Hawaii":"HI", "Idaho":"ID", "Illinois":"IL", "Indiana":"IN", "Iowa":"IA", "Kansas":"KS", "Kentucky":"KY", "Louisiana":"LA", "Maine":"ME", "Maryland":"MD", "Massachusetts":"MA", "Michigan":"MI", "Minnesota":"MN", "Mississippi":"MS", "Missouri":"MO", "Montana":"MT", "Nebraska":"NE", "Nevada":"NV", "New Hampshire":"NH", "New Jersey":"NJ", "New Mexico":"NM", "New York":"NY", "North Carolina":"NC", "North Dakota":"ND", "Ohio":"OH", "Oklahoma":"OK", "Oregon":"OR", "Pennsylvania":"PA", "Rhode Island":"RI", "South Carolina":"SC", "South Dakota":"SD", "Tennessee":"TN", "Texas":"TX", "Utah":"UT", "Vermont":"VT", "Virginia":"VA", "Washington":"WA", "West Virginia":"WV", "Wisconsin":"WI", "Wyoming":"WY"};
 const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+const years = {"2010":1, "2011": 2, "2012": 3, "2013": 4, "2014": 5, "2015": 6, "2016": 7, "2017": 8, "2018": 9, "2019": 10, "2020": 10}
 
 var data = [];
 var state_data = {};
+var pop = [];
+var year;
 
 $( function() {
     // Init datepicker
@@ -23,18 +26,29 @@ $( function() {
 function updateMap() {
     // Delete old map
     $("#map").html("");
-    
+
     // Load data for date
     loadData();
 }
 
 function loadData() {
-    var result = Papa.parse(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/` + 
+  year = ($("#date").val()).substring(6);
+//  console.log("year: "+ year);
+   var population_data = Papa.parse(`https://raw.githubusercontent.com/samanthalevin1/finalproject/master/nst-est2019-01.csv`, {
+      download: true,
+      complete: function(results, file){
+        pop = results.data;
+
+       }
+     });
+    var result = Papa.parse(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/` +
         `csse_covid_19_daily_reports/${$("#date").val()}.csv`, {
         download: true,
         complete: function(results, file) {
 	        data = results.data;
-            createMap();
+
+        console.log("data is: " + data);
+        createMap();
         }
     });
 }
@@ -47,7 +61,7 @@ function createMap() {
     var map_data = {};
 
     // Sort data by state
-    console.log(data);
+   console.log(data);
     data.forEach(state => {
         if (states.includes(state[2])) {
             if (!map_data[state_abbrevs[state[2]]]) {
@@ -59,7 +73,16 @@ function createMap() {
             }
         }
     })
-    
+
+    // Add population data to the map
+    console.log(pop);
+      pop.forEach(s => {
+        map_data[state_abbrevs[s[0]]] = {"population": s[years[year]]};
+    //  console.log("population: " + s[years[year]])
+        console.log(map_data[state_abbrevs[s[0]]]);
+      });
+
+
     // Create map UI
     var datamap = new Datamap({
         scope: "usa",
@@ -68,8 +91,8 @@ function createMap() {
         data: map_data,
         geographyConfig: {
             popupTemplate: function(geo, data) {
-                var popup = [`<div class="hoverinfo"><strong>${geo.properties.name}</strong><br>Confirmed cases: ${data.confirmed}` + 
-                            `<br>Deaths: ${data.deaths}</div>`];
+                var popup = [`<div class="hoverinfo"><strong>${geo.properties.name}</strong><br>Confirmed cases: ${data.confirmed}` +
+                            `<br>Deaths: ${data.deaths}` + `<br>Population: ${data.population}</div>`];
 
                 return popup;
             }
